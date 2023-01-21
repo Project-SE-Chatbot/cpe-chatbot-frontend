d6202afb6075600c5d46551eb45cb0670742ed38
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2023-01-21 15:17:20 +0700

    plain chat window

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import config from "./chatbotConfig";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
+import { Chatbot } from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="app-chatbot-container">
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+          </div>
       </header>
     </div>
   );
