f883298bb955bc9e9c962a2118c4dac23a6a6674
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2023-02-22 11:58:57 +0700

    new Adminpage/AdminNavbar

---
+++ src/AdminPages/Admin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const AdminPage = (props) => {
+  return (
+    <div className="admin-background">
+      <Navbar/>
+      
+    </div>
+  );
+};
+
+export default AdminPage;
---
+++ src/AdminPages/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logout from "../img/logout.png";
+import user from "../img/user.png";
+
+const Navbar = (props) => {
+  return (
+    <div className="admin-navbar-container">
+      <h1>CPE CHATBOT</h1>
+      <div className="admin-info-container">
+        <div className="admin-info-container">
+        <img src={user} style={{width: "35px",height: "35px",margin:"10px"}}></img>
+        <p>Benyapa k.</p>
+        </div>
+        <button className="admin-loguot-button">
+            <img src={logout} style={{width: "35px",height: "35px"}}></img>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
