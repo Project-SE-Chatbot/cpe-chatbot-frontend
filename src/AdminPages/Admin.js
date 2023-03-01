7e77d91a1843f70134eb9011567296e5590aff2f
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2023-03-01 20:02:07 +0700

    done pop up create

--- src/AdminPages/Admin.js
+++ src/AdminPages/Admin.js
@@ -1,12 +1,21 @@
 import React from "react";
 import AnsContainer from "./AnsContainer";
+import ConfirmDelAns from "./ConfirmDelAns";
 import Navbar from "./Navbar";
+import PopupCreAns from "./PopupCreAns";
+import EditDegree from "./PopupEditAns/EditDegree";
+import EditFreeElective from "./PopupEditAns/EditFreeElective";
+import EditMajor from "./PopupEditAns/EditMajor";
+import EditPlace from "./PopupEditAns/EditPlace";
+import EditPlan from "./PopupEditAns/EditPlan";
+import EditTeacher from "./PopupEditAns/EditTeacher";
 
 const AdminPage = (props) => {
   return (
     <div className="admin-background">
       <Navbar/>
       <AnsContainer/>
+      <PopupCreAns/>
     </div>
   );
 };
--- src/AdminPages/PopupCreAns.js
+++ src/AdminPages/PopupCreAns.js
@@ -2,37 +2,55 @@ import React from "react";
 import close from "../img/close (3).png";
 import InputInfo from "./inputInfo";
 import { useState } from 'react';
+import CreateTeacher from "./CreateAnsType/CreateTeacher";
+import CreateFreeElective from "./CreateAnsType/CreateFreeElective";
+import CreatePlace from "./CreateAnsType/CreatePlace";
+import CreatePlan from "./CreateAnsType/CreatePlan";
+import CreateDegree from "./CreateAnsType/CreateDegree";
+import CreateMajor from "./CreateAnsType/CreateMajor";
 
 const PopupCreAns = (props) => {
-    const button = {"teacher":"freeelective"};
     const [type, setType] = useState("teacher");
     const handleClick = (e) => {
         setType(e.target.id)
-      };
+    };
     return (
         <div className="admin-create-ans-box">
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
                 <button className="admin-button">
-                <img src={close} style={{ width: "15px", height: "15px" }}></img>
+                    <img alt="close" src={close} style={{ width: "15px", height: "15px" }}></img>
                 </button>
             </div>
             <h3>Create Answer</h3>
             <div >
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                    <div className={type!="teacher"?"admin-type-answr-box":"admin-type-answr-box-selected"} id="teacher"
+                    <div className={type !== "teacher" ? "admin-type-answr-box" : "admin-type-answr-box-selected"} id="teacher"
                         onClick={handleClick}>Teacher</div>
-                    <div className={type!="freeelective"?"admin-type-answr-box":"admin-type-answr-box-selected"} id="freeelective"
-                         onClick={handleClick}>Free Elective</div>
-                    <div className={type!="place"?"admin-type-answr-box":"admin-type-answr-box-selected"} id="place"
-                         onClick={handleClick}>Place</div>
-                    <div className={type!="degree"?"admin-type-answr-box":"admin-type-answr-box-selected"} id="degree"
-                         onClick={handleClick}>Degree</div>
-                    <div className={type!="plan"?"admin-type-answr-box":"admin-type-answr-box-selected"} id="plan"
-                         onClick={handleClick}>Plan</div>
-                    <div className={type!="major"?"admin-type-answr-box":"admin-type-answr-box-selected"} id="major"
-                         onClick={handleClick}>Major</div>
+                    <div className={type !== "freeelective" ? "admin-type-answr-box" : "admin-type-answr-box-selected"} id="freeelective"
+                        onClick={handleClick}>Free Elective</div>
+                    <div className={type !== "place" ? "admin-type-answr-box" : "admin-type-answr-box-selected"} id="place"
+                        onClick={handleClick}>Place</div>
+                    <div className={type !== "degree" ? "admin-type-answr-box" : "admin-type-answr-box-selected"} id="degree"
+                        onClick={handleClick}>Degree</div>
+                    <div className={type !== "plan" ? "admin-type-answr-box" : "admin-type-answr-box-selected"} id="plan"
+                        onClick={handleClick}>Plan</div>
+                    <div className={type !== "major" ? "admin-type-answr-box" : "admin-type-answr-box-selected"} id="major"
+                        onClick={handleClick}>Major</div>
                 </div>
-                <InputInfo/>
+                <div className="admin-create-ans-box-info-container">
+                    {type === "teacher"
+                        ? <CreateTeacher/>
+                        : type === "freeelective"
+                            ? <CreateFreeElective/>
+                            : type === "place"
+                                ? <CreatePlace/>
+                                : type === "degree"
+                                    ? <CreateDegree/>
+                                    : type === "plan"
+                                        ? <CreatePlan/>
+                                        : <CreateMajor/>
+                    }
+                </div>
             </div>
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
                 <div className="admin-create-answer-cancle-button ">Cancle</div>
