ba710fc93d34546645a9d6b5ef0687992027a18a
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2023-03-07 11:12:25 +0700

    del thing unused

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import './App.css';
-import { useState } from "react";
-import config from "./chatbotConfig";
-import Fade from "react-reveal/Fade";
-import Flip from "react-reveal/Flip";
-import MessageParser from "./MessageParser";
-import ActionProvider from "./ActionProvider";
-import { Chatbot } from "react-chatbot-kit";
 import 'react-chatbot-kit/build/main.css';
-import background from "./img/CPEWebMockup.png";
-import BotAvatar from "./img/robot.png";
-import cpebg from "./img/logo-cpe.jpg";
 import { Route, Routes } from 'react-router-dom';
 import AdminPage from './AdminPages/Admin';
 import Home from './Home';
