b240bbff2df664e8ebcc3128fc4797f2d4aa27f4
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2023-03-11 11:40:17 +0700

    put score to database

--- src/widgets/reaction.js
+++ src/widgets/reaction.js
@@ -13,114 +13,124 @@ const Reaction = ({ message }) => {
   const [isFourDisable, setisFourDisable] = useState(false);
   const [isFiveClicked, setisFiveClicked] = useState(false);
   const [isFiveDisable, setisFiveDisable] = useState(false);
+  const [score, setScore] = useState(0);
 
-  var scoreFeedbackUrl = ""
-  // const [score,setScore] = useState(0);
   const handleClick = (e) => {
-    let a ;
+    let s ;
     if (e.target.className.includes("one")) {
+      s = 1;
       setisOneClicked(true);
       setisTwoDisable(true);
       setisThreeDisable(true);
       setisFourDisable(true);
       setisFiveDisable(true);
-      console.log(message,1) ;
+      console.log(message, s);
     } else if (e.target.className.includes("two")) {
+      s = 2;
       setisTwoClicked(true);
       setisOneDisable(true);
       setisThreeDisable(true);
       setisFourDisable(true);
       setisFiveDisable(true);
-      console.log(message,2) ;
+      console.log(message, s);
     } else if (e.target.className.includes("three")) {
+      s = 3;
       setisThreeClicked(true);
       setisOneDisable(true);
       setisTwoDisable(true);
       setisFourDisable(true);
       setisFiveDisable(true);
-      console.log(message,3) ;
+      console.log(message, s);
     } else if (e.target.className.includes("four")) {
+      s = 4;
       setisFourClicked(true);
       setisOneDisable(true);
       setisTwoDisable(true);
       setisThreeDisable(true);
       setisFiveDisable(true);
-      console.log(message,4) ;
-    }else if (e.target.className.includes("five")) {
+      console.log(message, s);
+    } else if (e.target.className.includes("five")) {
+      s = 5;
       setisFiveClicked(true);
       setisOneDisable(true);
       setisTwoDisable(true);
       setisThreeDisable(true);
       setisFourDisable(true);
-      console.log(message,5) ;
+      console.log(message, s);
     }
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ question: "loveeee",
+      answer: message,
+      score: s,
+      feedback: "love" })
+  };
+  fetch('http://localhost:5000/feed-back', requestOptions)
+      .then(response => response.json())
+      .then(data => this.setState({ postId: data.id }));
   };
   const handleClassStyler = (buttonType) => {
     if (buttonType === 'one') {
       if (isOneClicked === true) {
-        // setScore(1)
         return "reaction-button OneClicked";
-      } else if (isOneDisable === true ) {
+      } else if (isOneDisable === true) {
         return "reaction-button disable";
       } else if (isOneClicked === false && isOneDisable === false) {
         return "reaction-button one";
       }
-    } else if (buttonType === 'two'){
+    } else if (buttonType === 'two') {
       if (isTwoClicked === true) {
-        // setScore(2)
         return "reaction-button TwoClicked";
-      } else if (isTwoDisable === true ) {
+      } else if (isTwoDisable === true) {
         return "reaction-button disable";
       }
       else if (isTwoClicked === false && isTwoDisable === false) {
         return "reaction-button two";
       }
-    }else if (buttonType === 'three'){
+    } else if (buttonType === 'three') {
       if (isThreeClicked === true) {
-        // setScore(3)
         return "reaction-button ThreeClicked";
-      } else if (isThreeDisable === true ) {
+      } else if (isThreeDisable === true) {
         return "reaction-button disable";
       }
       else if (isThreeClicked === false && isThreeDisable === false) {
         return "reaction-button three";
       }
-    }else if (buttonType === 'four'){
+    } else if (buttonType === 'four') {
       if (isFourClicked === true) {
-        // setScore(4)
         return "reaction-button TwoClicked";
-      } else if (isFourDisable === true ) {
+      } else if (isFourDisable === true) {
         return "reaction-button disable";
       }
       else if (isFourClicked === false && isFourDisable === false) {
         return "reaction-button four";
       }
-    }else if (buttonType === 'five'){
+    } else if (buttonType === 'five') {
       if (isFiveClicked === true) {
-        // setScore(5)
         return "reaction-button FiveClicked";
-      } else if (isFiveDisable === true ) {
+      } else if (isFiveDisable === true) {
         return "reaction-button disable";
       }
       else if (isFiveClicked === false && isFiveDisable === false) {
         return "reaction-button five";
       }
     }
-    };
+  };
   return (
     <div className="reaction-container">
       <div className="help">Was this help?</div>
       <div className="reaction-button-container">
-      <button disabled={isOneDisable}
-        onClick={handleClick} className={handleClassStyler('one')}>1</button>
-      <button disabled={isTwoDisable}
-        onClick={handleClick} className={handleClassStyler('two')}>2</button>
-      <button disabled={isTwoDisable}
-        onClick={handleClick} className={handleClassStyler('three')}>3</button>
-      <button disabled={isTwoDisable}
-        onClick={handleClick} className={handleClassStyler('four')}>4</button>
-      <button disabled={isTwoDisable}
-        onClick={handleClick} className={handleClassStyler('five')}>5</button>
+        <button disabled={isOneDisable}
+          onClick={handleClick} className={handleClassStyler('one')}>1</button>
+        <button disabled={isTwoDisable}
+          onClick={handleClick} className={handleClassStyler('two')}>2</button>
+        <button disabled={isTwoDisable}
+          onClick={handleClick} className={handleClassStyler('three')}>3</button>
+        <button disabled={isTwoDisable}
+          onClick={handleClick} className={handleClassStyler('four')}>4</button>
+        <button disabled={isTwoDisable}
+          onClick={handleClick} className={handleClassStyler('five')}>5</button>
       </div>
     </div>
   );
